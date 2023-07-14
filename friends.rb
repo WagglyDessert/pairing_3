711c258e52779391bcf48e4c408d6eb0ecdfdb02
Nathan Trautenberg <Natester123@mac.com>
2023-07-14 08:21:04 -0700

    Add second method

--- friends.rb
+++ friends.rb
@@ -7,3 +7,14 @@ def above_thirty(any_array)
     end
 end
 above_thirty(array)
+
+# method
+array = [20, 40, 60, 80, 100, 10, 30, 50]
+def below_sixty(any_array)
+    any_array.each do |element|
+        if element < 60
+            puts element
+        end
+    end
+end
+below_sixty(array)
